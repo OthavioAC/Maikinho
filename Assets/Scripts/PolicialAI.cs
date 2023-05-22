787ea6278a966d01f8b3294910a791532bd5f221
Vini-Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-05-22 12:19:07 -0300

    230509 - escalada inimigos
    - inimigos agora podem escalar quaisquer paredes ao perseguir maikinho

--- Assets/Scripts/PolicialAI.cs
+++ Assets/Scripts/PolicialAI.cs
@@ -15,12 +15,37 @@ public class PolicialAI : MonoBehaviour
     [SerializeField] Transform maicon;
     private SpriteRenderer copSprite;
     private Rigidbody2D copBody;
+    private BoxCollider2D copCollider;
     private PolicialAIState currentAIState;
-    private float copSpeed = 20f; // ph
+    private TipoMovimento currentMovimento;
+    // ph
+    public void SetMovimento(TipoMovimento novoTipo)
+    {
+        currentMovimento = novoTipo;
+        switch (novoTipo)
+        {
+            case TipoMovimento.Livre:
+                copBody.gravityScale = 1;
+                break;
+            case TipoMovimento.EscaladaOmnidirecional:
+                copBody.gravityScale = 0;
+                break;
+        }
+    }
+
+    private float copSpeed = 20f; // ph -> [4 - 6]
+    private float copClimbSpeed = 5f; // ph -> [6 - 8]
 
     private Transform target;
 
     private float phTimer = 2f;
+
+    private GameObject escalavelDisponivel = null; // mudar pra lista dps... ou bool, mas acho que lista
+
+    public void SetEscalavel(GameObject newEscalavel)
+    {
+        escalavelDisponivel = newEscalavel;
+    }
 
     public void SetTarget(Transform newTarget)
     {
@@ -34,10 +59,12 @@ public class PolicialAI : MonoBehaviour
 
     private void Start()
     {
-        Physics2D.IgnoreCollision(maicon.GetChild(1).GetComponent<BoxCollider2D>(), this.GetComponentInChildren<BoxCollider2D>(), true); // PH
+        copCollider = this.GetComponentInChildren<BoxCollider2D>();
+        Physics2D.IgnoreCollision(maicon.GetChild(1).GetComponent<BoxCollider2D>(), copCollider, true); // PH, usar PhysicsLayer dps
         copBody = GetComponent<Rigidbody2D>();
         copSprite = this.GetComponent<SpriteRenderer>();
         currentAIState = PolicialAIState.Idle;
+        currentMovimento = TipoMovimento.Livre;
     }
 
     private void Update()
@@ -55,11 +82,25 @@ public class PolicialAI : MonoBehaviour
             case PolicialAIState.Busca:
                 if (target != null)
                 {
-                    float currentDistance = target.position.x - this.transform.position.x;
-                    copSprite.flipX = currentDistance < 0 ? true : (currentDistance > 0 ? false : copSprite.flipX);
-                    if (Mathf.Abs(currentDistance) > 1f && copBody.velocity.magnitude < copSpeed)
+                    Vector2 currentDirection = target.position - this.transform.position;
+                    if (currentMovimento == TipoMovimento.Livre)
                     {
-                        copBody.velocity += new Vector2(currentDistance, 0f).normalized * copSpeed * Time.deltaTime;
+                        float currentDistance = currentDirection.x;
+                        copSprite.flipX = currentDistance < 0 ? true : (currentDistance > 0 ? false : copSprite.flipX);
+                        if (Mathf.Abs(currentDistance) > 3f && copBody.velocity.magnitude < copSpeed)
+                        {
+                            copBody.velocity += new Vector2(currentDistance, 0f).normalized * copSpeed * Time.deltaTime;
+                        }
+
+                        if(escalavelDisponivel != null && Mathf.Abs(currentDistance) < 3f && currentDirection.y > 2f)
+                        {
+                            this.SetMovimento(TipoMovimento.EscaladaOmnidirecional);
+                            copCollider.isTrigger = true;
+                        }
+                    }
+                    if (currentMovimento == TipoMovimento.EscaladaOmnidirecional)
+                    {
+                        this.transform.position += (Vector3)(currentDirection + Vector2.up).normalized * copSpeed * Time.deltaTime;
                     }
                 }
                 break;
--- Assets/Scripts/PolicialColliderController.cs
+++ Assets/Scripts/PolicialColliderController.cs
@@ -4,5 +4,42 @@ using UnityEngine;
 
 public class PolicialColliderController : MonoBehaviour
 {
+    private BoxCollider2D copCollider;
+    private PolicialAI copAI;
+
+    private void Start()
+    {
+        copCollider = this.GetComponent<BoxCollider2D>();
+        copAI = this.GetComponentInParent<PolicialAI>();
+    }
     /* AKI VAO OS ONTRIGGER E ONCOLLISION PRA DETECTAR CONTATO COM CASAS E COM O MAIKIN */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("PegouMaikin");
+        }
+        if (collision.gameObject.CompareTag("Casa"))
+        {
+            copAI.SetEscalavel(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (this.transform.position.y > collision.transform.position.y)
+            {
+                copCollider.isTrigger = false;
+                copAI.SetMovimento(TipoMovimento.Livre);
+            }
+        }
+        if (collision.gameObject.CompareTag("Casa"))
+        {
+            copCollider.isTrigger = false;
+            copAI.SetEscalavel(null);
+            copAI.SetMovimento(TipoMovimento.Livre);
+        }
+    }
 }
