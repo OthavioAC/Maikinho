bfcbc1208e48b3d0c16773b6df4bc7fa55040279
Vini-Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-05-25 13:07:20 -0300

    230525 - menu modificado
    - adicionada uma breve interacao animada antes de selecionar play

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Transform menuSelector;
+    [SerializeField] private Transform textoInicial;
+    private float menuAnimationFrame = 0f;
+    private float menuAnimationTime = 0.3f;
+    private bool selected = false;
+    private float eixo;
+
     private void Awake()
     {
         Screen.SetResolution(1280, 1200, true);
@@ -17,9 +24,35 @@ public class MainMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("MENU"))
+        if (Input.GetButton("MENU"))
         {
-            SceneManager.LoadScene("Rua");
+            if (menuAnimationFrame < menuAnimationTime) { menuAnimationFrame += Time.deltaTime; }
+            else menuAnimationFrame = menuAnimationTime;
+        }
+        else
+        {
+            if (menuAnimationFrame > 0f) { menuAnimationFrame -= Time.deltaTime; }
+            else menuAnimationFrame = 0f;
+        }
+
+        textoInicial.localScale = Vector3.one * (1 - (menuAnimationFrame / menuAnimationTime));
+        menuSelector.localScale = Vector3.one * (menuAnimationFrame / menuAnimationTime);
+
+        eixo = Input.GetAxis("VERTICAL0") + Input.GetAxis("VERTICAL1");
+        eixo = eixo > 1 ? 1 : (eixo < -1 ? -1 : eixo); // cap
+        if (selected && eixo == 0) selected = false;
+        if (menuAnimationFrame / menuAnimationTime == 1)
+        {
+            if (eixo >= 1 && !selected)
+            {
+                selected = true;
+                SceneManager.LoadScene("Rua");
+            }
+            if (eixo <= -1 && !selected)
+            {
+                selected = true;
+                Debug.Log("SAI DO JOOJ");
+            }
         }
     }
 }
