53a9e8b8bc85c26a418722167480197df30bc811
Vini-Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-05-25 13:02:25 -0300

    230525 - sistema de nuvens finalizado

--- Assets/Scripts/NuvemController.cs
+++ Assets/Scripts/NuvemController.cs
@@ -6,14 +6,20 @@ public class NuvemController : MonoBehaviour
 {
     [SerializeField] private GameObject nuvemPrefab;
     [SerializeField] private Transform nuvemContainer;
+    private Sprite[] nuvemSprite;
 
     private float nuvemCooldown;
+    private float bufferAltura = 20f;
+
+    private float alturaInicial = 20f;
+    private float rangeAltura = 200f;
 
     private void Start()
     {
         Vector3 bufferPosition = this.transform.position;
         bufferPosition.x = Camera.main.ViewportToWorldPoint(Vector3.left).x;
         this.transform.position = bufferPosition;
+        nuvemSprite = Resources.LoadAll<Sprite>("NUVEM");
         nuvemCooldown = 1f;
     }
 
@@ -22,10 +28,13 @@ public class NuvemController : MonoBehaviour
         nuvemCooldown -= Time.deltaTime;
         if(nuvemCooldown <= 0f)
         {
-            nuvemCooldown = .1f;
+            float novaAltura = Random.Range(alturaInicial, alturaInicial + rangeAltura);
+            nuvemCooldown = Random.Range(.1f, 1f) * (1 - Mathf.Abs((bufferAltura - novaAltura) / rangeAltura));
+            bufferAltura = novaAltura;
             GameObject novaNuvem = Instantiate(nuvemPrefab, nuvemContainer, false);
-            novaNuvem.transform.position = new Vector3(Camera.main.ViewportToWorldPoint(Vector3.right * 2f).x, Random.Range(20f, 40f), 0f);
-            novaNuvem.GetComponent<Rigidbody2D>().velocity = Vector2.left * Random.Range(10f, 35f);
+            novaNuvem.transform.position = new Vector3(Camera.main.ViewportToWorldPoint(Vector3.right * 2f).x, novaAltura, 0f);
+            novaNuvem.GetComponent<SpriteRenderer>().sprite = nuvemSprite[Random.Range(0, nuvemSprite.Length)];
+            novaNuvem.GetComponent<Rigidbody2D>().velocity = Vector2.left * Random.Range(5f, 20f);
         }
     }
 
