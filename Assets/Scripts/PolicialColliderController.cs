1968ce2be0378fd3eddd102eb79546606e27de8e
Vini-Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-06-05 17:32:16 -0300

    230605 - audio inimigos adicionado

--- Assets/Scripts/PolicialColliderController.cs
+++ Assets/Scripts/PolicialColliderController.cs
@@ -7,21 +7,30 @@ public class PolicialColliderController : MonoBehaviour
     private BoxCollider2D copCollider;
     private PolicialAI copAI;
 
+    private AudioClip hitDamage;
+    private AudioClip stunDamage;
+
     private void Start()
     {
         copCollider = this.transform.parent.GetChild(0).GetComponent<BoxCollider2D>();
         copAI = this.GetComponentInParent<PolicialAI>();
+
+        hitDamage = Resources.Load<AudioClip>("Audio/Dano");
+        stunDamage = Resources.Load<AudioClip>("Audio/Taser");
     }
     /* AKI VAO OS ONTRIGGER E ONCOLLISION PRA DETECTAR CONTATO COM CASAS E COM O MAIKIN */
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.tag.StartsWith("Ground"))
         {
             copAI.isGrounded = true;
         }
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log(copAI.HitPlayer(1) ? "Pegou Maikin" : "ERROU");
+            if (copAI.HitPlayer(1))
+            {
+                this.GetComponent<AudioSource>().PlayOneShot(copAI.GetTipoPolicial() == TipoPolicial.Civil ? hitDamage : stunDamage);
+            }
         }
         if (collision.gameObject.CompareTag("Casa"))
         {
@@ -31,7 +40,7 @@ public class PolicialColliderController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.tag.StartsWith("Ground"))
         {
             copAI.isGrounded = false;
             if (this.transform.position.y > collision.transform.position.y)
